42dc048059844575c8dba0e9c8c97dec42a46079
ishmaell <isiaka.ismaila@gmail.com>
2022-04-07 21:30:47 +0100

    chore: add big-hero

--- src/components/BigHero.tsx
+++ src/components/BigHero.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
 const BigHero: React.FC = () => {
-  return <h1>Big Hero</h1>;
+  return (
+    <section className="big-hero">
+      <div className="container">
+        <div className="cta-card">
+          <h1>Find inbound call centers for your company</h1>
+          <p>Use our AI and Big Data driven call center sourcing solution</p>
+        </div>
+        <div className="stats">
+          <h2 className="stats-header">
+            Welcome to Europe's largest call center database
+          </h2>
+          <div className="stats-info">
+            <div className="stats-info-item">
+              <h3>
+                500+ <span>Tenders</span>
+              </h3>
+            </div>
+            <div className="stats-info-item">
+              <h3>
+                200+ <span>Callcenter</span>
+              </h3>
+            </div>
+            <div className="stats-info-item">
+              <h3>
+                375.000 <span>Agents available</span>
+              </h3>
+            </div>
+            <div className="stats-info-item">
+              <h3>
+                85% <span>Faster sourcing</span>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BigHero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-bg': "url('/src/assets/images/big-hero.png')",
+      },
+      colors: {
+        "snubes-dark-gray": "#445566",
+      }
+    },
   },
   plugins: [],
 }
