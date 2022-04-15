09830fafcff06ef9aa58ab84381a45a7d10d6961
ishmaell <isiaka.ismaila@gmail.com>
2022-04-15 22:05:02 +0100

    fix: responsive layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import stamp from '../assets/images/stamp.png';
+import { RiFacebookFill, RiTwitterFill, RiLinkedinFill } from 'react-icons/ri';
 
 const Footer: React.FC = () => {
   const year = new Date().getFullYear() - 3;
@@ -23,9 +24,27 @@ const Footer: React.FC = () => {
               <a href="mailto:support@snubes.com">support@snubes.com</a>
               <a href="tel:+4903055645327">+49 (0) 305 5645327</a>
               <div className="socials">
-                <a href="./">F</a>
-                <a href="./">T</a>
-                <a href="./">L</a>
+                <a
+                  href="https://www.facebook.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <RiFacebookFill />
+                </a>
+                <a
+                  href="https://www.twitter.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <RiTwitterFill />
+                </a>
+                <a
+                  href="https://www.linkedin.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <RiLinkedinFill />
+                </a>
               </div>
             </div>
           </div>
